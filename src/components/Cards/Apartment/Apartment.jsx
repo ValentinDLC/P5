5810820e7920c94457dbe92d022a391732a31557
ValentinDLC <70979146+ValentinDLC@users.noreply.github.com>
2024-09-19 09:54:28 +0200

    Mise en page du site via la maquette figma

---
+++ src/components/Cards/Apartment/Apartment.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Apartment.scss"
+
+function Apartment() {
+  return (
+    <div className='location'>
+      <div className='apartment_subtitle'>Titre de la<br/> location</div>
+    </div>
+  )
+}
+
+export default Apartment
